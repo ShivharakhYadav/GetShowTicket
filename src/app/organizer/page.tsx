0a7a5cfbdc234c2221ff0634f4348afcf5b6c967
ShivharakhYadav <yadavshivharakhyadav@gmail.com>
2024-07-03 21:46:20 +0530

    Formated

--- src/app/organizer/page.tsx
+++ src/app/organizer/page.tsx
@@ -1,10 +1,10 @@
 const Organizer = () => {
   return (
     <div className="container mx-auto p-4">
-      <div className=" te">
+      <div className="te">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Events</h1>
-          <button className="bg-gray-500 rounded-md p-2 text-white">
+          <button className="rounded-md bg-gray-500 p-2 text-white">
             Add Event
           </button>
         </div>
