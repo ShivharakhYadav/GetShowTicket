d8ba75bc14035d589feee94f6ff9dd34ea4ae1e4
ShivharakhYadav <yadavshivharakhyadav@gmail.com>
2024-08-04 22:51:47 +0530

    Seat Layout Functinality Adde

--- src/components/pages/seat-booking/index.tsx
+++ src/components/pages/seat-booking/index.tsx
@@ -1,21 +1,26 @@
 "use client";
 
 import React, { FC, useEffect, useState } from "react";
+import clsx from "clsx";
 
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 
 import { useSeatBooking } from "./hooks";
+const defaultClasses =
+  "font-normal mx-0.5 my-0.5 h-6 w-3 px-3 max-[640px]:h-6 max-[640px]:w-2 max-[640px]:px-3 text-black bg-inherit border-black border-2 pointer-events-none cursor-none";
 
 interface Seat {
   id: string | null;
   name: string | number | null;
   isSeat: boolean;
+  available: boolean;
+  reserved: boolean;
 }
 
 const SeatBooking: FC = () => {
-  const { ApiResponse } = useSeatBooking();
+  const { ApiResponse, isSelectedSeat, handleSeatClick } = useSeatBooking();
 
   const [mounted, setMounted] = useState(false);
 
@@ -27,13 +32,10 @@ const SeatBooking: FC = () => {
     return null;
   }
 
-  const handleSeatClick = (seatId: string) => {
-    console.log("Selected seat:", seatId);
-    // Implement seat selection logic here
-  };
-
   return (
-    <main className="flex flex-1 flex-col gap-4 px-4 max-[640px]:p-1 lg:gap-6 lg:px-4">
+    <main
+    // className="flex flex-1 flex-col gap-4 px-4 max-[640px]:p-1 lg:gap-6 lg:px-4"
+    >
       <Card className="mx-auto w-full border-0">
         <CardContent className="flex flex-col gap-4 max-[640px]:p-0">
           <div className="hide-scrollbar flex items-center justify-center">
@@ -55,20 +57,37 @@ const SeatBooking: FC = () => {
                           {rowId}
                         </TableCell>
                         <TableCell className="p-1">
-                          {seats.map((seat: Seat, i: number) => (
-                            <Button
-                              key={`${seat.id}-${i}`}
-                              onClick={() =>
-                                seat.isSeat &&
-                                seat.id &&
-                                handleSeatClick(seat.id)
-                              }
-                              disabled={!seat.isSeat}
-                              className="mx-0.5 my-0.5 h-6 w-3 px-3 max-[640px]:h-6 max-[640px]:w-2 max-[640px]:px-3"
-                            >
-                              {/* {seat.name ? seat.name : <>&nbsp;</>} */}
-                            </Button>
-                          ))}
+                          {seats.map((seat: Seat, i: number) => {
+                            if (!seat.isSeat)
+                              return (
+                                <Button
+                                  key={`${seat.id}-${i}`}
+                                  className={`${defaultClasses} border-transparent hover:bg-inherit`}
+                                >
+                                  &nbsp;
+                                </Button>
+                              );
+
+                            const btnClasses = clsx(defaultClasses, {
+                              "reserved relative": seat.reserved,
+                              available: !seat.reserved && seat.available,
+                              booked: !seat.reserved && !seat.available,
+                              selected: seat.id && !isSelectedSeat(seat.id),
+                            });
+                            return (
+                              <Button
+                                key={`${seat.id}-${i}`}
+                                onClick={() =>
+                                  seat.isSeat &&
+                                  seat.id &&
+                                  handleSeatClick(seat.id)
+                                }
+                                className={btnClasses}
+                              >
+                                {seat.name ? seat.name : <>&nbsp;</>}
+                              </Button>
+                            );
+                          })}
                         </TableCell>
                       </TableRow>
                     ))}
@@ -76,6 +95,28 @@ const SeatBooking: FC = () => {
                 ))}
               </TableBody>
             </Table>
+          </div>
+          <div className="flex items-center justify-center gap-1">
+            <div className="flex items-center gap-1">
+              <div className="pointer-events-none h-6 w-3 cursor-none rounded-md border-2 border-black bg-inherit px-3 font-normal text-black max-[640px]:h-6 max-[640px]:w-2 max-[640px]:px-3"></div>
+              <span>available</span>
+            </div>
+            <div className="flex items-center gap-1">
+              <div className="bg-booked pointer-events-none h-6 w-7 cursor-none rounded-md"></div>
+              <span>booked</span>
+            </div>
+            <div className="flex items-center gap-1">
+              <div
+                className={`${defaultClasses} details reserved relative h-6 w-7 cursor-none rounded-md`}
+              >
+                &nbsp;
+              </div>
+              <span>reserved</span>
+            </div>
+            <div className="flex items-center gap-1">
+              <div className="bg-available pointer-events-none h-6 w-7 cursor-none rounded-md"></div>
+              <span>selected</span>
+            </div>
           </div>
           {/* <Button
             type="button"
