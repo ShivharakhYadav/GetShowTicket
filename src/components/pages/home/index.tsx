75f341b75fbdada1b58e3320d13ee81d6fea2feb
Gaurav Siddhapathaki <90097984+GauravS13@users.noreply.github.com>
2024-11-01 12:45:58 +0530

    fix: Removed warning from carousel and console

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -24,7 +24,7 @@ const Home = () => {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-        <Carousel autoPlay infiniteLoop showStatus={false}>
+        <Carousel autoPlay infiniteLoop showStatus={false} showThumbs={false}>
           {BANNER_IMAGES.map((imageUrl, index) => (
             <div
               className="relative h-64 w-full md:h-96"
